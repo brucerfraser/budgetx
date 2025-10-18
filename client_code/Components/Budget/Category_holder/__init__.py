fa35a7895b828b7ff4a4a00af012e501c95fde81
bruce.fraserb@gmail.com <bruce.fraserb@gmail.com>
2025-10-18 18:36:45 +0000

    Edited Pop_menus.work_a_category, Components.Budget.Category_holder and Components.Budget.Category_holder.Sub_category

--- client_code/Components/Budget/Category_holder/__init__.py
+++ client_code/Components/Budget/Category_holder/__init__.py
@@ -17,7 +17,23 @@ class Category_holder(Category_holderTemplate):
 
   def link_2_click(self, **event_args):
     from ....Pop_menus.work_a_category import work_a_category
-    c = work_a_category()
+    c = work_a_category(cat=False)
     result = alert(c,title="Add a sub-category to {cat}".format(cat=self.item['name']),
                   buttons=[],large=True)
-    print(result, self.item['category_id'])
+    if result:
+      result['belongs_to'] = self.item['category_id']
+      for a in ['colour_back','colour_text']:
+        del result[a]
+      app_tables.sub_categories.add_row(**result)
+      if self.link_1.icon == 'fa:angle-down':
+        self.repeating_panel_1.items = app_tables.sub_categories.search(belongs_to=self.item['category_id'])
+
+
+  def link_1_click(self, **event_args):
+    if self.link_1.icon == 'fa:angle-right':
+      self.link_1.icon = 'fa:angle-down'
+      self.repeating_panel_1.items = app_tables.sub_categories.search(belongs_to=self.item['category_id'])
+      self.repeating_panel_1.visible = True
+    else:
+      self.link_1.icon = 'fa:angle-right'
+      self.repeating_panel_1.visible = False
--- client_code/Pop_menus/work_a_category/__init__.py
+++ client_code/Pop_menus/work_a_category/__init__.py
@@ -9,9 +9,13 @@ import anvil.js
 import time
 
 class work_a_category(work_a_categoryTemplate):
-  def __init__(self, **properties):
+  def __init__(self, cat=True,**properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
+    self.cat = cat
+    if not self.cat:
+      for thing in [self.flow_panel_1,self.flow_panel_2,self.label_1,self.label_2]:
+        thing.visible = False
     self.colorpicker_back.set_color('#C0C9C1')
     self.colorpicker_text.set_color("#000000")
     self.label.text = "Choose name"
@@ -30,10 +34,11 @@ class work_a_category(work_a_categoryTemplate):
   def button_2_click(self, **event_args):
     if len(self.label.text) > 2 and not self.label.text in ["Choose name",'Income']:
       from ... import Global
+      id = 'category_id' if self.cat else 'sub_category_id'
       self.raise_event('x-close-alert',value={'name':self.label.text,
                                               'colour_back':self.colorpicker_back.get_color(),
                                              'colour_text':self.colorpicker_text.get_color(),
-                                             'category_id':Global.new_id_needed()})
+                                             id:Global.new_id_needed()})
     else:
       self.button_2.background = 'theme:Error'
       time.sleep(2)
@@ -44,5 +49,14 @@ class work_a_category(work_a_categoryTemplate):
 
   def text_box_1_pressed_enter(self, **event_args):
     self.label.text = self.text_box_1.text
+    if not self.cat:
+      self.button_2_click()
+
+  def text_box_1_update(self, **event_args):
+    self.label.text = self.text_box_1.text
+
+  def form_show(self, **event_args):
+    self.text_box_1.focus()
+    self.text_box_1.select()
 
     
