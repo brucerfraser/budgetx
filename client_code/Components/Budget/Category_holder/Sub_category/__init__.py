35ed1a1fb52c453e48be61f7143a57c43846a33d
bruce.fraserb@gmail.com <bruce.fraserb@gmail.com>
2025-10-26 08:45:55 +0000

    Edited Components.Budget, settings and Components.Budget.Category_holder.Sub_category

--- client_code/Components/Budget/Category_holder/Sub_category/__init__.py
+++ client_code/Components/Budget/Category_holder/Sub_category/__init__.py
@@ -17,28 +17,37 @@ class Sub_category(Sub_categoryTemplate):
     today = date.today()
     period = date(today.year, today.month, 1)
     try:
-      self.budget.text = app_tables.budgets.search(period=period,
-                                                   belongs_to=self.item['sub_category_id'])[0]['budget_amount']
+      self.budget.text = str(app_tables.budgets.search(period=period,
+                            belongs_to=self.item['sub_category_id'])[0]['budget_amount'])
+      self.budget_edit.text = float(self.budget.text)
     except:
-      self.budget.text = 0
+      self.budget.text = "0"
+      self.budget_edit.text = 0
 
   def budget_edit_lost_focus(self, **event_args):
     today = date.today()
     period = date(today.year, today.month, 1)
-    if not self.budget.text == 0:
+    if not self.budget_edit.text == 0:
       try:
         app_tables.budgets.get(period=period,
-                               belongs_to=self.item['sub_category_id'])[0]['budget_amount'] = self.budget.text
+                               belongs_to=self.item['sub_category_id'])[0]['budget_amount'] = self.budget_edit.text
       except:
         app_tables.budgets.add_row(belongs_to=self.item['sub_category_id'],
-                                period=period,budget_amount=self.budget.text)
+                                period=period,budget_amount=self.budget_edit.text)
+      self.budget.text = str(self.budget_edit.text)
 
   def link_1_click(self, **event_args):
     # Fire something to budget form for RH panel.
-
+    frame = anvil.get_open_form()
+    budg = frame.content_panel.get_components()[0]
+    today = date.today()
+    period = date(today.year, today.month, 1)
+    budg.load_category_right(self.item['sub_category_id'],period)
     # Make all other sub_cats clickable again (links vis, edit invis)
+    budg.reset_sub_categories(self.item['sub_category_id'])
 
     # Edit column_panel becomes visual.
-    
+    self.edit_column_panel.visible = True
+    self.link_1.visible = False
     """This method is called when the link is clicked"""
     pass
--- client_code/Components/Budget/__init__.py
+++ client_code/Components/Budget/__init__.py
@@ -13,6 +13,8 @@ class Budget(BudgetTemplate):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
     inc_d = {}
+    self.category_right = ""
+    self.period_right = None
     for inc in app_tables.categories.search(name='Income'):
       inc_d = dict(inc)
     self.card_2.add_component(Category_holder(item=inc_d))
@@ -49,5 +51,37 @@ class Budget(BudgetTemplate):
         cat_d = dict(cat)
         self.card_expenses.add_component(Category_holder(my_identity=cat_d))
     
+  def test_me(self, **event_args):
+    print("works!")
+
+  def load_category_right(self,cat,period, **event_args):
+    self.category_right,self.period_right = cat,period
+    nts = None
+    try:
+      nts = app_tables.budgets.get(period=period,
+                             belongs_to=cat)['notes']
+    except Exception as e:
+      print("error:",e)
+      
+    sc = app_tables.sub_categories.get(sub_category_id=cat)['name']
+    c_id = app_tables.sub_categories.get(sub_category_id=cat)['belongs_to']
+    c = app_tables.categories.get(category_id=c_id)['name']
+    self.label_2.text = c + " - " + sc
+    self.notes.text = nts
+
+  def reset_sub_categories(self,cat,**event_args):
+    for category in self.expense_categories.get_components():
+      # print(category)
+      if category.link_1.icon == "fa:angle-down":
+        #sub-cates are open
+        for sub_cat in category.repeating_panel_1.get_components():
+          if sub_cat.item["sub_category_id"] != cat:
+            sub_cat.edit_column_panel.visible = False
+            sub_cat.link_1.visible = True
+        
+
+  def update_notes(self, **event_args):
+    app_tables.budgets.get(belongs_to=self.category_right,
+                           period=self.period_right)['notes'] = self.notes.text
     
     
